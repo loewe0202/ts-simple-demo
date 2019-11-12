88640e95be5d88afe8c3c9555328d65b6a99c451
loewe0202 <loewe0202@163.com>
2019-11-13 00:04:08 +0800

    ts class

--- HelloWord.js
+++ HelloWord.js
@@ -12,3 +12,58 @@ var XiaoJieJie = /** @class */ (function () {
 var jiejie = new XiaoJieJie('范冰冰', 18);
 console.log(jiejie);
 jiejie.say();
+var XiaoJieJie2 = /** @class */ (function () {
+    function XiaoJieJie2(sex, name, age) {
+        this.sex = sex;
+        this.name = name;
+        this.age = age;
+    }
+    XiaoJieJie2.prototype.sayHello = function () {
+        console.log('小哥哥好');
+    };
+    XiaoJieJie2.prototype.sayLove = function () {
+        console.log('我爱你');
+    };
+    return XiaoJieJie2;
+}());
+var jiejie2 = new XiaoJieJie2('女', '热巴', 22);
+console.log(jiejie2.sex);
+// console.log(jiejie2.name)   //报错
+// console.log(jiejie2.age)    //报错
+jiejie2.sayHello();
+var myhusband = { sex: '男', interest: '看书、作家务', maiBaoBao: true };
+console.log(myhusband);
+var mySearch = function (source, subString) {
+    var flag = source.search(subString);
+    return (flag != -1);
+};
+console.log(mySearch('高、富、帅、德', '胖')); //false
+var shuaiGe;
+(function (shuaiGe) {
+    var Dehua = /** @class */ (function () {
+        function Dehua() {
+            this.name = '刘德华';
+        }
+        Dehua.prototype.talk = function () {
+            console.log('我是帅哥刘德华');
+        };
+        return Dehua;
+    }());
+    shuaiGe.Dehua = Dehua;
+})(shuaiGe || (shuaiGe = {}));
+var bajie;
+(function (bajie) {
+    var Dehua = /** @class */ (function () {
+        function Dehua() {
+            this.name = '马德华';
+        }
+        Dehua.prototype.talk = function () {
+            console.log('我是二师兄马德华');
+        };
+        return Dehua;
+    }());
+    bajie.Dehua = Dehua;
+})(bajie || (bajie = {}));
+var dehua1 = new shuaiGe.Dehua();
+var dehua2 = new bajie.Dehua();
+dehua1.talk();
--- HelloWord.ts
+++ HelloWord.ts
@@ -12,3 +12,70 @@ class XiaoJieJie {
 let jiejie: XiaoJieJie = new XiaoJieJie('范冰冰', 18)
 console.log(jiejie)
 jiejie.say()
+
+class XiaoJieJie2 {
+    public sex: string
+    protected name: string
+    private age: number
+    public constructor (sex: string, name: string, age: number) {
+        this.sex = sex
+        this.name = name
+        this.age = age
+    }
+    public sayHello () {
+        console.log('小哥哥好')
+    }
+    protected sayLove () {
+        console.log('我爱你')
+    }
+}
+
+var jiejie2: XiaoJieJie2 = new XiaoJieJie2('女', '热巴', 22)
+
+console.log(jiejie2.sex)
+// console.log(jiejie2.name)   //报错
+// console.log(jiejie2.age)    //报错
+jiejie2.sayHello()
+// jiejie2.sayLove()    //报错
+
+interface Husband {
+    sex: string
+    interest: string
+    maiBaoBao?: Boolean
+}
+let myhusband: Husband = { sex: '男', interest: '看书、作家务', maiBaoBao: true }
+console.log(myhusband)
+
+interface SearchMan {
+    (source: string, subString: string): boolean
+}
+
+let mySearch: SearchMan = (source: string, subString: string): boolean => {
+    let flag = source.search(subString)
+    return (flag != -1)
+}
+
+console.log(mySearch('高、富、帅、德', '胖')) //false
+
+
+namespace shuaiGe {
+    export class Dehua {
+        public name: string = '刘德华'
+        talk () {
+            console.log('我是帅哥刘德华')
+        }
+    }
+}
+
+namespace bajie {
+    export class Dehua {
+        public name: string = '马德华'
+        talk () {
+            console.log('我是二师兄马德华')
+        }
+    }
+}
+
+let dehua1: shuaiGe.Dehua = new shuaiGe.Dehua()
+let dehua2: shuaiGe.Dehua = new bajie.Dehua()
+dehua1.talk()
